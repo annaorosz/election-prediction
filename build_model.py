d6f2780960c5d3ea418f26bd4085413a899e5a5f
panni <aorosz@sas.upenn.edu>
2017-10-02 23:33:16 -0400

    added txt files

--- build_model.py
+++ build_model.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
 from keras.backend.tensorflow_backend import set_session
 from keras.layers import Dense, Activation, Dropout
 from keras.layers.normalization import BatchNormalization
@@ -13,28 +8,15 @@ import numpy as np
 import random as rn
 import tensorflow as tf
 
-
-# In[2]:
-
-# make Keras take only the required amount of memory from the GPU and not allow
-config = tf.ConfigProto()
-#config.gpu_options.per_process_gpu_memory_fraction = 0.2
-config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-set_session(tf.Session(config=config))
-
-
-# In[ ]:
-
 trainfile = "train_potus_by_county.csv"
 reader = np.genfromtxt(trainfile, delimiter=',')
 x_train = reader[1:,:-1]
 
-# create shuffled ids
+#shuffle ids
 ids = list(range(x_train.shape[0]))
 rn.shuffle(ids)
 
 x_train = x_train[ids]
-
 
 with open(trainfile) as f:
     reader = csv.reader(f)
@@ -42,12 +24,13 @@ with open(trainfile) as f:
     y_train = np.array(y_train)
     y_train = y_train[ids]  # randomize
 
-
-# Single layer neural network model
+#layers of the model
 model = Sequential()
+#first layer
 model.add(Dense(32, input_dim=14))  # input_dim: num of features
 model.add(BatchNormalization())
 model.add(Activation('relu'))
+#hidden layers
 model.add(Dense(64))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
@@ -68,14 +51,13 @@ model.add(Activation('relu'))
 model.add(Dense(32))
 model.add(BatchNormalization())
 model.add(Activation('relu'))
+#output layer
 model.add(Dense(1, activation='sigmoid'))
-
 
 # configure the learning process
 model.compile(loss='binary_crossentropy',
           optimizer=SGD(lr=0.1, decay=1e-6),
           metrics=['accuracy'])
-
 
 # x_train and y_train are Numpy arrays --just like in the Scikit-Learn API.
 model.fit(x_train, y_train, epochs=10000, batch_size=1000, validation_split=0.1,
@@ -83,14 +65,3 @@ model.fit(x_train, y_train, epochs=10000, batch_size=1000, validation_split=0.1,
      )
 
 model.save('my_model.h5')
-
-
-# In[ ]:
-
-
-
-
-# In[ ]:
-
-
-
--- make_predictions.py
+++ make_predictions.py
@@ -1,8 +1,3 @@
-
-# coding: utf-8
-
-# In[1]:
-
 from keras.backend.tensorflow_backend import set_session
 from keras.layers import Dense, Activation, Dropout
 from keras.layers.normalization import BatchNormalization
@@ -13,32 +8,17 @@ import numpy as np
 import random as rn
 import tensorflow as tf
 
-
-
-# In[11]:
-
-
-
-
-# In[25]:
-
 testfile = "test_potus_by_county.csv"
 reader = np.genfromtxt(testfile, delimiter=',')
 x_predict = reader[1:,:]
 
+#load model binary that was built in build_model
 m = load_model('my_model.h5')
 
+#predict winners based on the model
 y_predict = m.predict(x_predict, batch_size=100, verbose=1)
 
-
-# In[29]:
-
-#print out the number of Obamas
-print(len([x for x in y_predict if x<=0.5]))
-
-
-# In[30]:
-
+#write the winners to precictions.csv
 file = open("predictions.csv", "w")
 file.write("Winner\n")
 for x in np.nditer(y_predict):
@@ -46,9 +26,3 @@ for x in np.nditer(y_predict):
         file.write("Mitt Romney\n") 
     else: 
         file.write("Barack Obama\n")
-
-
-# In[ ]:
-
-
-
